bfdf19a3956f2b0de9f424472ef7aa1bd722dddf
CathyAkoth <catherinececilia22@gmail.com>
2021-10-30 21:47:06 -0700

    Adding Logic to backdrop

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -18,11 +18,14 @@ function Todo(props){
         <div className="actions">
             <button className="btn" onClick={deleteHandler}>Delete</button>
     </div>
-    {modalIsOpen && <Modal /> }
+    {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>
+        )}
     {modalIsOpen && <Backdrop onClick={closeModalHandler} /> }
    
   </div>
-    );
+   
+    
+    )};
 
-}
 export default Todo;
